8149cd29f4f521d8963817f24e47848ab69d458d
jose daniel puc poot <josedaniel.pucpoot@gmail.com>
2023-10-14 12:01:13 -0700

    correccion de espacios

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 int size_char = sizeof(char);
@@ -8,10 +12,10 @@ int size_long = sizeof(long int);
 int size_long_long = sizeof(long long int);
 int size_float = sizeof(float);
 
-printf("Size of a char: %ld  byte(s)\n",size_char);
-printf("Size of an int: %ld byte(s)\n",size_int);
-printf("Size of a long int: %ld byte(s)\n",size_long);
-printf("Size of a long long int: %ld byte(s)\n",size_long_long);
-printf("Size of a float: %ld byte(s)\n",size_float);
+printf("Size of a char: %ld byte(s)\n", size_char);
+printf("Size of an int: %ld byte(s)\n", size_int);
+printf("Size of a long int: %ld byte(s)\n", size_long);
+printf("Size of a long long int: %ld byte(s)\n", size_long_long);
+printf("Size of a float: %ld byte(s)\n", size_float);
 return (0);
 }
