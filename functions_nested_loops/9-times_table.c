df41d9b5d3266db64466c044702cfca6bcbe1b21
jose daniel puc poot <josedaniel.pucpoot@gmail.com>
2023-10-21 13:00:24 -0700

    tabla

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * times_table - print table 10 x 10
+ * main - code
+ *
+ */
+void times_table(void)
+{
+int a, b, c;
+for (a = 0; a <= 9; a++)
+{
+for (b = 0; b <= 9; b++)
+{
+c = a * b;
+if (c == 0 && b == 0)
+_putchar(c + '0');
+else if (c > 9)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(c + '0');
+}
+else
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(c + '0');
+}
+}
+_putchar('\n');
+}
+}
