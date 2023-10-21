ed30c2eeeb850c637aa6a0c6764725c6c3289e03
jose daniel puc poot <josedaniel.pucpoot@gmail.com>
2023-10-21 11:27:41 -0700

    print the last digit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_last_digit - extract last number
+ * @i: variable
+ * main - code
+ * Return: i
+ */
+int print_last_digit(int i)
+{
+i %= 10;
+if (i < 0)
+{
+i *= -1;
+}
+_putchar(i);
+return (i - '0');
+}
