95c97b339623c451db95f3dab4f2da4baf170a67
jose daniel puc poot <josedaniel.pucpoot@gmail.com>
2023-10-20 22:27:42 -0700

    for y for

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+int alpha;
+
+for (n = '0'; n <= '9'; n++)
+{
+putchar(n);
+}
+for (alpha = 'a'; alpha <= 'f'; alpha++)
+{
+putchar(alpha);
+}
+putchar('\n');
+return (0);
