50839e2ebb2e45cfaf25b38d882961a06ab50463
jose daniel puc poot <josedaniel.pucpoot@gmail.com>
2023-10-21 16:55:24 -0700

    check is digit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isdigit - checks for a digit
+ * @c: variable or digit
+ * main - code
+ * Return: checks
+ */
+int _isdigit(int c)
+{
+int i;
+if (c >= 0 && c <= 9)
+{
+i = 1;
+}
+else
+{
+i = 0;
+}
+return (i);
+}
