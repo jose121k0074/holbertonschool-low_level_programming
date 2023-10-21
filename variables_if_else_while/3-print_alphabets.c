37e086479504a120702f07869b36e6ddb00300b8
jose daniel puc poot <josedaniel.pucpoot@gmail.com>
2023-10-20 21:16:03 -0700

    abecedario minus y mayus

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *	alphabet
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char alpha;
+
+for (alpha = 'a'; alpha<= 'z'; alpha++)
+{
+putchar(abc);
+}
+for (alpha = 'A'; alpha <= 'Z'; alpha++)
+{
+putchar(alpha);
+}
+putchar('\n');
+return (0);
+}
