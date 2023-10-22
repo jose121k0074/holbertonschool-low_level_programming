93bf844e5c67b11277ca6ef0f8e9a57ef9b01d42
jose daniel puc poot <josedaniel.pucpoot@gmail.com>
2023-10-21 20:03:48 -0700

    more numbers

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * more_numbers - print all numers x10
+ */
+void more_numbers(void)
+{
+int i;
+int x;
+for (i = 0; i < 10; i++)
+{
+for (x = 0; x <= 14; x++)
+{
+if (x > 9)
+{
+_putchar((x / 10) + '0');
+_putchar((x % 10) + '0');
+}
+}
+_putchar('\n');
+}
+}
