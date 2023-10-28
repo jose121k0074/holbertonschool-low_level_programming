e863e2041cdb88503f10aef836f1f7b4cb56884b
jose daniel puc poot <josedaniel.pucpoot@gmail.com>
2023-10-28 09:54:44 -0700

    reset to 98

---
+++ pointers_arrays_string/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - reset to 98
+ * @n: pointer whose value will be changed
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ pointers_arrays_string/README.md
@@ -0,0 +1,3 @@
+files:
+- 0-reset_to_98.c - reset variable to 98 value.
+- 1-swap
