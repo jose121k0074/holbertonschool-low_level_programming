564d54e0ecbc24f50bbdeafa2a3b336bbc44f2c4
jose daniel puc poot <josedaniel.pucpoot@gmail.com>
2023-10-29 10:26:42 -0700

    linea mas de 80 charcater

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -2,8 +2,8 @@
 
 /**
  * cap_string - capitalize
- * @a: variable a
- * Return: a
+ * @str: variable str
+ * Return: str
  */
 
 char *cap_string(char *str)
@@ -15,7 +15,10 @@ char *cap_string(char *str)
 		while (!(str[i] >= 'a' && str[i] <= 'z'))
 			i++;
 
-		if (str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' || str[i - 1] == ',' ||
+		if (str[i - 1] == ' ' ||
+			str[i - 1] == '\t' ||
+			str[i - 1] == '\n' ||
+			str[i - 1] == ',' ||
 			str[i - 1] == ';' ||
 			str[i - 1] == '.' ||
 		    str[i - 1] == '!' ||
