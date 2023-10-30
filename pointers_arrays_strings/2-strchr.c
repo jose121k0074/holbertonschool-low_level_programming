0996c177d13bc36ae8b3b27ff5a8f34b8e48b035
jose daniel puc poot <josedaniel.pucpoot@gmail.com>
2023-10-29 19:06:39 -0700

    betty correcciones

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -9,16 +9,16 @@
 
 char *_strchr(char *s, char c)
 {
-	int i = 0;
+int i = 0;
 
-	while (s[i])
-	{
-	    if (c == s[i])
-		{
-			s += i;
-			return (s);
-		}
-		i++;
-	}
-	return ('\0');
+while (s[i])
+{
+if (s[i] == c)
+{
+s += i;
+return (s);
+}
+i++;
+}
+return ('\0');
 }
