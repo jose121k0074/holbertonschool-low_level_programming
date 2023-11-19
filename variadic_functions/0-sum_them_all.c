90de97a4d77c919a302454d635df2c6a5a408469
jose daniel puc poot <josedaniel.pucpoot@gmail.com>
2023-11-19 13:30:55 -0800

    corrections of betty

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -4,7 +4,7 @@
 /**
  * sum_them_all - sum of all its parameters
  * @n: number of parameters
- * Return: sum of parameters 
+ * Return: sum of parameters
  */
 
 int sum_them_all(const unsigned int n, ...)
