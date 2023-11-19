7f15ee982553dbb140c04d78ffbb7c1bd837babd
jose daniel puc poot <josedaniel.pucpoot@gmail.com>
2023-11-19 08:24:55 -0800

    dog es cabecera

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,19 @@
-struct dog{
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struct with elements name, age, owner
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: struct for dog
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
-}
+};
+
+#endif
